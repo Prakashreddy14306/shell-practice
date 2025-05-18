8b6e9272204240251b1e5239d33359adcc3a92e4
prakash <prakash@gmail.com>
2025-05-18 11:03:09 +0530

    shell-practice

--- 03-if-condition.sh
+++ 03-if-condition.sh
@@ -2,9 +2,17 @@
 
 num1=$1
 
-if [ $num1 -le 0 ]
+# if [ $num1 -le 0 ]
+# then
+#     echo "number is accepted"
+# else
+#     echo "enter valid number"
+# fi
+
+if [ $num1 -eq 0 ]
 then
-    echo "number is accepted"
-else
     echo "enter valid number"
+elif [ $num1 -gt 0 ]
+then
+    echo "correct number"
 fi
