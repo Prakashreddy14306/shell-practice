939a4bbb3f5edd01e2bdb8d78ac1dd64849ebbaf
prakash <prakash@gmail.com>
2025-05-18 10:54:11 +0530

    shell-practice

---
+++ 02-special-var.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+echo "all variables passed: $@"
+echo "total number of variables:$#"
+echo "script name:$0"
+echo "current process id:$$"
+sleep 60
+echo "previeous process id:$!"
+echo "exit status:$?"
